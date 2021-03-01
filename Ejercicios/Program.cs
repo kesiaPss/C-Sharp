252b51a44c4f1cd0cf88d7c7b7ca9c4a6711fdce
Kesia Peralta <marilyn16.peralta@gmail.com>
2021-03-01 10:56:18 -0600

    agregando hola mundo

---
+++ Ejercicios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
